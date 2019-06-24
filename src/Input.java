9c0df895bb4e077980dd4559d90018a09c7d74a1
Matt Kissell <matt.kissell@crunch.co.uk>
2019-06-24 15:41:30 +0100

    Input for number of players + code refactoring for first round

--- src/Input.java
+++ src/Input.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 
 public class Input {
-    public void startTournament() {
+    public int numberOfPlayers() {
+        Scanner numberReader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("How many players do you want to enter (max 64):  ");
+        int inputPlayers = numberReader.nextInt();
+
+        if(inputPlayers > 64) {
+            numberOfPlayers();
+        }
+        return inputPlayers;
+    }
+
+    public Round startTournament(Tournament newTournament, Round firstRound) {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Do you wish to start the first round? (yes/no) :  ");
         String start = reader.nextLine();
-        reader.close();
         if(start.equals("yes")) {
-            Round firstRound = new Round();
-            firstRound.generateRound(1);
+            firstRound.generateRound(1, newTournament);
             System.out.println("-------------------------------");
-            System.out.println(firstRound.getRoundName());
+            System.out.println(firstRound.getRoundName() + " - Round " + (firstRound.getRoundNumber()));
         } else if (start.equals("no")) {
             System.exit(0);
         } else if(!start.equals("yes") || !start.equals("no")) {
-            startTournament();
+            startTournament(newTournament, firstRound);
         }
-        ;
+
+        return firstRound;
+
     }
+
 }
