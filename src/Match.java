4ae4c9fcebe9ec95adcce64e45d64164d917e281
Matt Kissell <matt.kissell@crunch.co.uk>
2019-07-03 15:22:50 +0100

    Removed template code for match structure

--- src/Match.java
+++ src/Match.java
@@ -1,39 +1,3 @@
-//import java.util.Set;
-//
-//public class Match {
-//    private final Set<Player> team1;
-//    private final Set<Player> team2;
-//    private MatchResult result;
-//
-//    public Match(Set<Player> team1, Set<Player> team2) {
-//        this.team1 = team1;
-//        this.team2 = team2;
-//    }
-//
-//    public Set<Player> getTeam1() {
-//        return team1;
-//    }
-//
-//    public Set<Player> getTeam2() {
-//        return team2;
-//    }
-//
-//    /**
-//     * <p>
-//     * The result of a match. WON means team1 won. LOST means team1 lost.
-//     * </p>
-//     */
-//    public MatchResult getResult() {
-//        return result;
-//    }
-//
-//    public void setResult(MatchResult result) {
-//        this.result = result;
-//    }
-//}
-
-import javafx.beans.binding.ObjectExpression;
-
 import java.util.ArrayList;
 
 public class Match {
