5396e46394e3fd2be51e1f0bbcb45fb6bc0c7fe5
Matt Kissell <matt.kissell@crunch.co.uk>
2019-07-03 15:15:17 +0100

    New public pauseTournament for those wishing not to continue + spacing

--- src/Input.java
+++ src/Input.java
@@ -6,23 +6,27 @@ public class Input {
         System.out.println("How many players do you want to enter (max 64):  ");
         int inputPlayers = numberReader.nextInt();
 
-        if(inputPlayers > 64) {
+        if (inputPlayers > 64) {
             numberOfPlayers();
         }
         return inputPlayers;
+    }
+
+    public void pauseTournament() {
+
     }
 
     public Round startTournament(Tournament newTournament, Round firstRound) {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Do you wish to start the first round? (yes/no) :  ");
         String start = reader.nextLine();
-        if(start.equals("yes")) {
+        if (start.equals("yes")) {
             firstRound.generateRound(1, newTournament);
             System.out.println("-------------------------------");
             System.out.println(firstRound.getRoundName() + " - Round " + (firstRound.getRoundNumber()));
         } else if (start.equals("no")) {
             System.exit(0);
-        } else if(!start.equals("yes") || !start.equals("no")) {
+        } else if (!start.equals("yes") || !start.equals("no")) {
             startTournament(newTournament, firstRound);
         }
 
