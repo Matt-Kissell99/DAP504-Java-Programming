851ae346b3fb8a6abfde5f2a68e6af7c3d2c513c
Matt Kissell <kissellmj15@gmail.com>
2019-07-14 22:10:55 +0100

    Inherited Games class

--- src/Match.java
+++ src/Match.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Match {
+public class Match extends Games {
     ArrayList<Match> roundMatches = new ArrayList<Match>();
     private Player Player1;
     private Player Player2;
@@ -38,13 +38,48 @@ public class Match {
         }
 
         this.roundMatches.forEach(Match -> {
-            System.out.println(Match.getPlayer1().getFirstName() + " " + Match.getPlayer1().getLastName() + " " + "VS " + Match.getPlayer2().firstName + " " + Match.getPlayer2().getLastName());
+            System.out.println(Match.getPlayer1().getFirstName() + " " + Match.getPlayer1().getLastName() + " " + "VS " + Match.getPlayer2().getFirstName() + " " + Match.getPlayer2().getLastName());
         });
     }
 
     public void playMatches(Tournament elimination) {
         this.roundMatches.forEach(Match -> {
-            Games playGames = new Games();
+            Games playGames = new Games() {
+                @Override
+                public int getGameNumber() {
+                    return super.getGameNumber();
+                }
+
+                @Override
+                public int getPlayer1GamesWon() {
+                    return super.getPlayer1GamesWon();
+                }
+
+                @Override
+                public int getPlayer2GamesWon() {
+                    return super.getPlayer2GamesWon();
+                }
+
+                @Override
+                public Player playGames(Match currentMatch, Tournament elimination) {
+                    return super.playGames(currentMatch, elimination);
+                }
+
+                @Override
+                public String playFirstGame(String commentary) {
+                    return super.playFirstGame(commentary);
+                }
+
+                @Override
+                public String playSecondGame(String commentary) {
+                    return super.playSecondGame(commentary);
+                }
+
+                @Override
+                public String playThirdGame(String commentary) {
+                    return super.playThirdGame(commentary);
+                }
+            };
             Player matchWinner = playGames.playGames(Match, elimination);
             elimination.addroundWaitingPlayers(matchWinner);
         });
