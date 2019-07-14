a69b6c670eb5def094ce664bf5fcbf12cd725947
Matt Kissell <kissellmj15@gmail.com>
2019-07-14 22:08:00 +0100

    Changed overview form

--- src/Overview.java
+++ src/Overview.java
@@ -3,86 +3,38 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
 public class Overview {
 
     public JPanel mainPanel;
-    public JButton startTournamentButton;
-    public JButton yesButton;
-    public JButton noButton;
-    public JButton getCurrentPlayerStatisticsButton;
-    public JButton endTournamentButton;
-    public JTextArea terminalArea;
     public JTextArea playerStatistics;
-    public JButton testingButtonButton;
-    public JCheckBox isThisWorkingCheckBox;
-    public JFormattedTextField iSwearThisIsFormattedTextField;
 
-    public Overview() {
-//        startTournamentButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//            }
-//        });
-//
-//        getCurrentPlayerStatisticsButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//            }
-//        });
+
+    public Overview(Tournament currentEvent) {
+
+        playerStatistics.append("hello");
+        checkPlayerStatistics(currentEvent);
     }
 
-    public void initialiseGUI() {
-        JFrame myFrame = new JFrame("Overview");
-        myFrame.setSize(10000, 10000);
-        myFrame.setContentPane(new Overview().mainPanel);
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.pack();
-        myFrame.setVisible(true);
+    public JFrame initialiseGUI(Tournament currentEvent) {
+        JFrame scoreBoard = new JFrame("Overview");
+        scoreBoard.setSize(50000, 50000);
+        scoreBoard.setContentPane(new Overview(currentEvent).mainPanel);
+        scoreBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        scoreBoard.pack();
+        scoreBoard.setVisible(true);
+        return scoreBoard;
     }
 
-
-    public void appendToTerminal(String sentence) {
-        terminalArea = new JTextArea();
-        terminalArea.setEditable(false);
-        terminalArea.append(sentence);
-    }
-
-    public JButton getNoButton() {
-        return noButton;
-    }
-
-    public JButton getStartTournamentButton() {
-        return startTournamentButton;
-    }
-
-    public JPanel getMainPanel() {
-        return mainPanel;
-    }
-
-    public JButton getEndTournamentButton() {
-        return endTournamentButton;
-    }
-
-    public JButton getGetCurrentPlayerStatisticsButton() {
-        return getCurrentPlayerStatisticsButton;
-    }
-
-    public JButton getYesButton() {
-        return yesButton;
-    }
-
-    public JTextArea getTerminalArea() {
-        return terminalArea;
-    }
-
-    public void startListener() {
-        startTournamentButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            }
-        });
+    public void checkPlayerStatistics(Tournament currentEvent) {
+        if(currentEvent.getRemainingPlayers().size() == 0) {
+            playerStatistics.setText("No Players found");
+        } else {
+            playerStatistics.setText("");
+            currentEvent.getRemainingPlayers().forEach(player -> {
+                playerStatistics.append(player.getFirstName() + " " + player.getLastName() + " - Age: " + player.getAge() + ", Skill Level: " + player.getSkillLevel() + ", Status: " + player.getPlayerStatus() + "\n");
+            });
+        }
     }
 }
