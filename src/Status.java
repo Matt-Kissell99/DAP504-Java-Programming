09fdf0c47404eacc9dff33730970597a43c66305
Matt Kissell <kissellmj15@gmail.com>
2019-07-15 22:23:39 +0100

    Status.java code comments

--- src/Status.java
+++ src/Status.java
@@ -1,14 +1,14 @@
 public enum Status {
-    ACTIVE ("Active"),
-    ELIMINATED ("Eliminated");
+    ACTIVE ("Active"), //Enum for player being active in the tournament
+    ELIMINATED ("Eliminated"); //Enum for player being eliminated from tournament
 
-    public String playerStatus;
+    public String playerStatus; //Variable to set enum
 
-    Status(String status) {
+    Status(String status) { //Setting the enum to the requested enum
         this.playerStatus = status;
     }
 
-    public String getPlayerStatus() {
+    public String getPlayerStatus() { //Getter method for roundStatus
         return playerStatus;
     }
 }
