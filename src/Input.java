a18a27a55a688b1d9cea39b780ad2dfee528582d
Matt Kissell <matt.kissell@crunch.co.uk>
2019-07-15 16:29:58 +0100

    Code comments on Input.java

--- src/Input.java
+++ src/Input.java
@@ -7,6 +7,7 @@ public class Input {
         System.out.println("How many players do you want to enter (4, 8, 16, 32, or 64):  ");
         int inputPlayers = numberReader.nextInt();
 
+        //If statement, if the number of players inputted is not equal to 64, 32, 16, 8, or 4 then return a message and loop back
         if (inputPlayers == 64) {
             return inputPlayers;
         } else if (inputPlayers == 32) {
@@ -21,7 +22,7 @@ public class Input {
             System.out.println("Please try and enter a valid number (4, 8, 16, 32, or 64)");
             numberOfPlayers();
         }
-        return inputPlayers;
+        return inputPlayers; //Return the number of players if it equals 64, 32, 16, 8, or 4
     }
 
     public Round startTournament(Round firstRound, Tournament currentTournament) {
@@ -44,9 +45,9 @@ public class Input {
 
     public void startMatches(Round round) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Do you wish to start the matches for round" + " " + round.getRoundNumber() + " - " + round.getRoundName() + " (y/n) :  ");
+        System.out.println("Do you wish to start the matches for round" + " " + round.getRoundNumber() + " - " + round.getRoundName() + " (y/n) :  "); //Asking users if they want to start the matches
         String startMatch = input.nextLine();
-        if (startMatch.equals("y")) {
+        if (startMatch.equals("y")) { //Simple if statement, if they enter "y" then it returns and begins the matches
             return;
         } else {
             this.startMatches(round);
